6bfb4ca01927c2e8dd22835133653df6e6c81327
71summernight <71summernight@naver.com>
2021-10-05 17:38:36 +0900

    초기셋팅 완료

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import '@fortawesome/fontawesome-free/js/all.js';
 import '../src/styles/common.scss';
 import '../src/styles/reset.scss';
 
