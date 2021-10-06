39f11617241f22363beb40520a490602e52c3b48
정경훈 <kyunghoon@jeong-gyeonghun-ui-MacBookAir.local>
2021-10-06 20:53:47 +0900

    [25기 정경훈] 로그인 레이아웃 완성, 기술구현 중

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+// import Signup from './pages/Signup';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
-        <Switch></Switch>
+        <Switch>
+          <Route exact path="/users/login" component={Login} />
+          {/* <Route exact path="/users/signup" component={Signup} /> */}
+        </Switch>
       </Router>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Routes from './Routes';
 import '../src/styles/common.scss';
 import '../src/styles/reset.scss';
+import '@fortawesome/fontawesome-free/js/all.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Routes />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Routes />, document.getElementById('root'));
