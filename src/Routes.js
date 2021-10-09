56d2997b38bcde00464f17b4e18b6cbe67a9806a
정경훈 <87818170+kyunghoon1017@users.noreply.github.com>
2021-10-09 11:39:00 +0900

    컴플릭트 해결중

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import Signup from './pages/Signup';
 
 class Routes extends React.Component {
@@ -8,6 +9,7 @@ class Routes extends React.Component {
     return (
       <Router>
         <Switch>
+          {/* <Route exact path = "users/login" component ={Login}/> */}
           <Route exact path="/users/signup" component={Signup} />
         </Switch>
       </Router>
