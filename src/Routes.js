14ab6c917781824f6f25fe613647eb0328891ad4
정경훈 <87818170+kyunghoon1017@users.noreply.github.com>
2021-10-09 15:02:48 +0900

    컨플릭트 제발 ㅠ

--- src/Routes.js
+++ src/Routes.js
@@ -1,25 +1,16 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-<<<<<<< HEAD
-import Login from '.pages/Login';
-import Signup from './pages/Signup';
-=======
 import Login from './pages/Login';
->>>>>>> feature/loginlayout
+import Signup from './pages/Signup';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-<<<<<<< HEAD
-          {/*<Route exact path="/users/login" componetn={Login} />*/}
-          <Route exact path="/users/signup" component={Signup} />
-=======
           <Route exact path="/users/signin" component={Login} />
-          {/* <Route exact path="/users/signup" component={Signup} /> */}
->>>>>>> feature/loginlayout
+          <Route exact path="/users/signup" component={Signup} />
         </Switch>
       </Router>
     );
