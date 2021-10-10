e241651b4f99c8319bbf1b2da9b8c5e041b14499
정경훈 <87818170+kyunghoon1017@users.noreply.github.com>
2021-10-10 21:44:00 +0900

    [25기 정경훈] 장바구니 레이아웃 완료

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Basket from './pages/Basket';
 
 class Routes extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class Routes extends React.Component {
         <Switch>
           <Route exact path="/users/signin" component={Login} />
           <Route exact path="/users/signup" component={Signup} />
+          <Route exact path="/baskets" component={Basket} />
         </Switch>
       </Router>
     );
