554f13043f69de3bec61bfff8faf910e770c2084
정경훈 <87818170+kyunghoon1017@users.noreply.github.com>
2021-10-14 16:36:09 +0900

    수정사항 반영완료 과연 이것이 맞을 것인가

--- src/Routes.js
+++ src/Routes.js
@@ -8,7 +8,7 @@ import SubjectList from './components/SubjectList/SubjectList';
 
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import Basket from './pages/Cart/Basket';
+// import Basket from './pages/Cart/Basket';
 
 class Routes extends React.Component {
   render() {
@@ -20,7 +20,7 @@ class Routes extends React.Component {
             <Route path="/Main" component={Main} />
             <Route exact path="/users/signin" component={Login} />
             <Route exact path="/users/signup" component={Signup} />
-            <Route exact path="/baskets" component={Basket} />
+            {/* <Route exact path="/baskets" component={Basket} /> */}
           </Switch>
           <Footer />
         </Router>
